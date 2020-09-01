eb5c580042d20d06afbf96a6c52398a08775ae56
GraysonHardin <61327747+GraysonHardin@users.noreply.github.com>
2020-08-31 20:12:10 -0500

    Wrote a program to show the quantity, discription, and cost of an item.

---
+++ week_2/constant.py
@@ -0,0 +1,3 @@
+QUANTITY = 10
+ITEM_DISCRIPTION = "pizzas will cost"
+COST = 15.00
---
+++ week_2/use_constant.py
@@ -0,0 +1,5 @@
+import constant
+
+print(constant.QUANTITY, end=' ')
+print(constant.ITEM_DISCRIPTION, end=' ')
+print(constant.COST)
